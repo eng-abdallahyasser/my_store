53f9b87785b9deec516295dbab37ce067f56eef5
ENG ABDALLAH YASSER <abdallahyasser1277@gmail.com>
2024-07-02 21:14:47 +0300

    "Added new colors to colors.dart, updated pubspec.yaml and pubspec.lock files"

--- lib/core/constant/colors.dart
+++ lib/core/constant/colors.dart
@@ -3,4 +3,9 @@ import 'package:flutter/material.dart';
 class MyColors {
   static const Color gray = Colors.grey;
   static const Color white = Color(0xfff8f9fd);
+  static const Color elsie = Color(0xff9d5d77);
+  static const Color elsieLite = Color(0xfff3bfbb);
+  static const Color matteCharcoal = Color(0xff444352);
+  
+
 }
